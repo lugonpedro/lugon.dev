9f1e79897f4e057f9ee18babe89a47642329bf87
Pedro Lugon <pedromotta20@gmail.com>
2021-04-15 23:18:18 -0300

    :ambulance: update about text

--- src/components/organisms/SectionAbout.jsx
+++ src/components/organisms/SectionAbout.jsx
@@ -15,11 +15,11 @@ function SectionAbout() {
       </a>
       <p>
         Nascido em 1998, sou um estudante de Sistemas da Informação pela
-        faculdade UDF, atualmente no último ano, tenho 22 anos e moro em
-        Brasília-DF, programo assiduamente há dois anos e faço livecoding na
-        twitch como hobbie, gosto muito de procurar aprender mais e participar
-        de projetos em que estejam dispostos a fazer coisas novas. Me considero
-        desenvolvedor back-end porém estou em busca de ser um bom fullstack.
+        faculdade UDF, atualmente no último ano, e moro em Brasília-DF, programo
+        assiduamente há dois anos e faço livecoding na twitch como hobbie, gosto
+        muito de procurar aprender mais e participar de projetos em que estejam
+        dispostos a fazer coisas novas. Me considero desenvolvedor back-end
+        porém estou em busca de ser um bom fullstack.
       </p>
       <div className={styles.icons}>
         <a target="_blank" rel="noreferrer" href="http://github.com/1ugon">
