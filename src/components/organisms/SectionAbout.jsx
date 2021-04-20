fb920d9cbadbcfbfd373a8d053ec5ed5576ceb66
Pedro Lugon <pedromotta20@gmail.com>
2021-04-19 22:23:57 -0300

    :bug: changing about

--- src/components/organisms/SectionAbout.jsx
+++ src/components/organisms/SectionAbout.jsx
@@ -15,11 +15,11 @@ function SectionAbout() {
       </a>
       <p>
         Nascido em 1998, sou um estudante de Sistemas da Informação pela
-        faculdade UDF, atualmente no último ano, e moro em Brasília-DF, programo
-        assiduamente há dois anos e faço livecoding na twitch como hobbie, gosto
-        muito de procurar aprender mais e participar de projetos em que estejam
-        dispostos a fazer coisas novas. Me considero desenvolvedor back-end
-        porém estou em busca de ser um bom fullstack.
+        faculdade UDF e moro em Brasília, programo assiduamente desde 2019 e
+        faço livecoding na twitch como hobbie, gosto muito de procurar aprender
+        mais e participar de projetos em que estejam dispostos a fazer coisas
+        novas. Me considero desenvolvedor back-end porém estou em busca de ser
+        um bom fullstack.
       </p>
       <div className={styles.icons}>
         <a target="_blank" rel="noreferrer" href="http://github.com/1ugon">
