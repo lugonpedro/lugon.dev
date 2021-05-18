b8f192a9b86166686bd9b65a28cf3667fda84fd1
Pedro Lugon <pedromotta20@gmail.com>
2021-05-17 21:38:32 -0300

    :sparkles: adding a project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,6 +3,7 @@ import autoescola from "../assets/autoescola.png";
 import mate from "../assets/mate.png";
 import onichan from "../assets/onichan.png";
 import lugbot from "../assets/lugbot.png";
+import festivaltf from "../assets/festivaltf.png";
 
 export const projects = [
   {
@@ -39,8 +40,7 @@ export const projects = [
     imagealt: "Onichan Bot",
     summary:
       "Bot de Discord feito com o Discord JS integrando também a API jikan-node que faz conexão com o My Anime List, sendo capaz de pegar perfis de usuários, animes, mangás, personagens, pessoas, e muitas coisas mais. Projeto open source feito por pura diversão e vontade de aprender tecnologias novas! Caso queira implementar features e participar do projeto entre em contato comigo pelas redes sociais ou o formulário do site.",
-    link:
-      "https://discord.com/oauth2/authorize?client_id=805520334111375410&scope=bot&permissions=8",
+    link: "https://discord.com/oauth2/authorize?client_id=805520334111375410&scope=bot&permissions=8",
     source: "https://github.com/1ugon/bot-onichan",
   },
   {
@@ -65,9 +65,20 @@ export const projects = [
     imagealt: "Lug Bot",
     summary:
       "Bot de Discord feito com o Discord JS integrado a API da Twitch.tv que recebe quando minha live fica ao vivo e avisa no servidor. Projeto open source feito por pura diversão, facilitação e vontade de aprender tecnologias novas!",
-    link:
-      "https://discord.com/oauth2/authorize?client_id=809327272381120533&scope=bot&permissions=8",
+    link: "https://discord.com/oauth2/authorize?client_id=809327272381120533&scope=bot&permissions=8",
     source: "https://github.com/1ugon/lugbot",
+  },
+  {
+    id: 6,
+    slang: "festivaltf",
+    title: "Festival Transfeminista",
+    description: "Site interativo criado para festival online",
+    image: festivaltf,
+    imagealt: "Festival Transfeminista",
+    summary:
+      "Site interativo para festival feito em React, de acordo com as necessidades e vontades do cliente, com o intuito de agregar valor ao festival que ocorre em Coimbra e precisava de um espaço online por questões pandêmicas.",
+    link: "https://www.festivaltransfeminista.com/",
+    source: "https://github.com/1ugon/festivaltf",
   },
 ];
 
