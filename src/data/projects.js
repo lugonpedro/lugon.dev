f3cd525c659f0206a079cd781290a871e4c43c59
Pedro Lugon <pedromotta20@gmail.com>
2021-07-18 01:57:55 -0300

    fixing metatags

--- src/data/projects.js
+++ src/data/projects.js
@@ -25,7 +25,7 @@ export const projects = [
     image: onichan,
     imagealt: "Onichan Bot",
     summary:
-      "Bot de Discord feito com o Discord JS integrando também a API jikan-node que faz conexão com o My Anime List, sendo capaz de pegar perfis de usuários, animes, mangás, personagens, pessoas, e muitas coisas mais. Projeto open source feito por pura diversão e vontade de aprender tecnologias novas! Caso queira implementar features e participar do projeto entre em contato comigo pelas redes sociais ou o formulário do site.",
+      "Bot de Discord feito com o Discord JS integrando também a API jikan-node que faz conexão com o My Anime List, sendo capaz de pegar perfis de usuários, animes, mangás, personagens, pessoas, e muitas coisas mais. Projeto open source feito por pura diversão e vontade de aprender tecnologias novas.",
     link: "https://discord.com/oauth2/authorize?client_id=805520334111375410&scope=bot&permissions=8",
     source: "https://github.com/1ugon/bot-onichan",
   },
