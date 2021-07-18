3b92584a58daea59d259bcac6a7df356c867fcdf
Pedro Lugon <pedromotta20@gmail.com>
2021-07-18 01:17:03 -0300

    adding SEO

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
+/* ReactDOM.render(
+  <React.StrictMode>  
     <App />
   </React.StrictMode>,
   document.getElementById("root")
-);
+); */
