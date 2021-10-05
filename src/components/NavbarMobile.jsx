36705a6b43c29c14701ca9a84a08bfa4674269b7
Pedro Lugon <pedromotta20@gmail.com>
2021-10-04 22:06:32 -0300

    removing navbar mobile fixed

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -1,64 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
 
 import styles from "../styles/NavbarMobile.module.css";
-import {
-  FaBars,
-  FaTimes,
-  FaUserAlt,
-  FaListAlt,
-  FaPencilRuler,
-  FaHistory,
-} from "react-icons/fa";
+import { FaUserAlt, FaListAlt, FaPencilRuler, FaHistory } from "react-icons/fa";
 
 import { Link } from "react-router-dom";
 
 function NavbarMobile() {
-  const [showing, setShowing] = useState(false);
-
   return (
-    <>
-      <div className={styles.mainButton}>
-        <span onClick={() => setShowing(!showing)}>
-          {showing ? <FaTimes size={30} /> : <FaBars size={30} />}
-        </span>
+    <nav className={styles.container}>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">
+              <span>
+                <FaUserAlt size={30} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/skills">
+              <span>
+                <FaListAlt size={30} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects">
+              <span>
+                <FaPencilRuler size={30} />
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/experience">
+              <span>
+                <FaHistory size={30} />
+              </span>
+            </Link>
+          </li>
+        </ul>
       </div>
-      {showing && (
-        <nav className={styles.container}>
-          <div>
-            <ul>
-              <li>
-                <Link to="/">
-                  <span>
-                    <FaUserAlt size={30} />
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/skills">
-                  <span>
-                    <FaListAlt size={30} />
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/projects">
-                  <span>
-                    <FaPencilRuler size={30} />
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/experience">
-                  <span>
-                    <FaHistory size={30} />
-                  </span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      )}
-    </>
+    </nav>
   );
 }
 
