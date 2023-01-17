f5b594ed6eafc25de3d2b1a7154d46fce3396dba
Pedro Lugon <pedromotta20@gmail.com>
2023-01-17 15:49:42 -0300

    Update Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Skill({ image, title }: Skill) {
   return (
-    <div className="group relative flex flex-col hover:text-neon">
+    <div className="group relative flex flex-col hover:scale-125 duration-300">
       <img
         src={image}
-        className="rounded-full border border-green object-cover w-20 h-20 md:h-24 md:w-24 xl:w-32 xl:h-32 hover:border-neon hover:scale-110 duration-300"
+        className="rounded-full border border-green object-cover w-20 h-20 md:h-24 md:w-24 xl:w-32 xl:h-32"
       />
       <p className="text-center mt-2">{title}</p>
     </div>
