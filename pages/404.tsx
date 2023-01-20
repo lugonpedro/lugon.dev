ab50dd1db3e81012d730c08d0ca7875131cfdea6
Pedro Lugon <pedromotta20@gmail.com>
2023-01-19 23:25:47 -0300

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 type Props = {};
 
@@ -7,12 +8,12 @@ export default function Page404({}: Props) {
     <div className="flex items-center justify-center flex-col h-screen w-screen bg-blue text-green">
       <h1 className="text-8xl">404</h1>
       <p className="text-xl mb-4">Página não encontrada</p>
-      <a
+      <Link
         href="/"
         className="border border-green rounded-md px-8 py-2 hover:bg-green duration-300 hover:text-neon font-bold"
       >
         Voltar
-      </a>
+      </Link>
     </div>
   );
 }
