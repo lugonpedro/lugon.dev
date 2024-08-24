5022d44f73426bae338cc7acb37fef17006a44b0
Pedro Lugon <pedromotta20@gmail.com>
2024-08-24 19:14:02 -0300

    fix(change language): add dry

--- src/components/change-language.tsx
+++ src/components/change-language.tsx
@@ -4,25 +4,20 @@ import { useTranslation } from "react-i18next";
 export function ChangeLanguage() {
   const { i18n } = useTranslation();
 
+  const selectedLang =
+    "text-primary/20 decoration-solid decoration-main pointer-events-none";
+
   return (
     <div className="flex gap-4 justify-end">
       <a
         onClick={() => changeLanguage("pt-BR")}
-        className={
-          i18n.language === "pt-BR"
-            ? "text-primary/20 decoration-solid decoration-main pointer-events-none"
-            : "cursor-pointer"
-        }
+        className={i18n.language === "pt-BR" ? selectedLang : "cursor-pointer"}
       >
         pt
       </a>
       <a
         onClick={() => changeLanguage("en")}
-        className={
-          i18n.language === "en"
-            ? "text-primary/20 decoration-solid pointer-events-none"
-            : "cursor-pointer"
-        }
+        className={i18n.language === "en" ? selectedLang : "cursor-pointer"}
       >
         en
       </a>
