b8ec28018b6be0ea534c12c9b8c75a647e9b9dbe
Pedro Lugon <pedromotta20@gmail.com>
2024-08-24 19:00:16 -0300

    fix(experiences): change layout

--- src/sections/experiences.tsx
+++ src/sections/experiences.tsx
@@ -9,12 +9,21 @@ export function Experiences() {
   return (
     <>
       <Separator />
-      <p className="font-semibold">{t("experiences.title")}</p>
-      <ul>
-        {data.map((exp) => (
-          <li key={exp}>{t(`experiences.${exp}.position`)}</li>
+      <h2 className="font-semibold text-2xl">{t("experiences.title")}</h2>
+      <div className="flex flex-col gap-4 mt-4">
+        {data.reverse().map((exp) => (
+          <div key={exp}>
+            <h6 className="font-semibold text-xl">{exp}</h6>
+            <p className="italic">{t(`experiences.${exp}.position`)}</p>
+            <div className="flex gap-1 text-sm font-extralight">
+              <span>{t(`experiences.${exp}.start_date`)}</span>
+              <p>-</p>
+              <span>{t(`experiences.${exp}.end_date`)}</span>
+            </div>
+            <p>{t(`experiences.${exp}.description`)}</p>
+          </div>
         ))}
-      </ul>
+      </div>
     </>
   );
 }
